d62fadf9712704f259ed15f4b9e4784e5a79a387
John <1>
2023-11-22 23:53:50 +0700

    make hw4

---
+++ src/main/java/Tickets/Account.java
@@ -0,0 +1,32 @@
+package Tickets;
+
+import java.text.DecimalFormat;
+
+public class Account {
+    private long userAccountId;
+    private DecimalFormat balance;
+
+    public Account(long userAccountId, DecimalFormat balance) {
+        this.userAccountId = userAccountId;
+        this.balance = balance;
+    }
+
+    public Account() {
+    }
+
+    public long getUserAccountId() {
+        return userAccountId;
+    }
+
+    public void setUserAccountId(long userAccountId) {
+        this.userAccountId = userAccountId;
+    }
+
+    public DecimalFormat getBalance() {
+        return balance;
+    }
+
+    public void setBalance(DecimalFormat balance) {
+        this.balance = balance;
+    }
+}
