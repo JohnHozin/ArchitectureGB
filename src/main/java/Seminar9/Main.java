fc20d2ddea2e99f702a8ec4861ad85f7bbac9afc
JohnHozin <simple_protector@mail.ru>
2023-12-20 13:36:07 +0300

    make seminar 9 done.

--- src/main/java/Seminar9/Main.java
+++ src/main/java/Seminar9/Main.java
@@ -1,5 +1,6 @@
 package Seminar9;
 
 public class Main {
-    // Заглушка для домашней работы
+    // Просмотр макета приложения
+    // https://www.figma.com/file/kMi6cF22BtJaEAsb89yXTP/UIUX-hosting?type=design&node-id=0-1&mode=design&t=9ikSmWRJ3Ufb3Vao-0
 }
